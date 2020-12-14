02a68b40c01ced2e831bb15e87f3a93a63eb0381
root <root@pan.bitvax.com>
2020-12-14 11:59:39 +0100

    Rest of modules

---
+++ availability/condition/maxviews/lang/en/availability_maxviews.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings.
+ *
+ * @package availability_maxviews
+ * @copyright 2015 Daniel Neis
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['ajaxerror'] = 'Error contacting server';
+$string['pluginname'] = 'Max Views';
+$string['title'] = 'Maximum Views';
+$string['description'] = 'Prevent access after user view the module a certain number of times.';
+$string['eithernotdescription'] = 'you have not reached the limit of {$a->viewslimit} views (you have {$a->viewscount} views)';
+$string['eitherdescription'] = 'you have reached the limit of {$a->viewslimit} views (you have {$a->viewscount} views)';
+$string['fieldlabel'] = 'Maximum views:';
+$string['validnumber'] = 'You must add a number higher than 0';
